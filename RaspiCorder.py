910d301834863b937e40b60fa9627e8653a3445a
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2015-04-30 19:20:10 -0400

    back to basics

--- RaspiCorder.py
+++ RaspiCorder.py
@@ -3,14 +3,5 @@
 from time import sleep
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 
-pos = 0
-
 lcd = Adafruit_CharLCDPlate.Adafruit_CharLCDPlate()
 lcd.begin(16,2)
-lcd.clear()
-lcd.message("  Introducing\n RaspiCorder")
-sleep(2)
-lcd.clear()
-
-lcd.blink()
-update()
