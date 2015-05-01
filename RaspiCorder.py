5b27668b01642fe95592ae43a823a713c7f1a869
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2015-05-01 12:53:40 -0400

    error fixed?

--- RaspiCorder.py
+++ RaspiCorder.py
@@ -3,9 +3,10 @@
 from time import sleep
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 
-# Initialization
+#Initialization of the plate
 lcd = Adafruit_CharLCDPlate.Adafruit_CharLCDPlate()
 lcd.begin(16,2)
-lcd.message("  Introducing\n RaspiCorder")
+lcd.clear()
+lcd.message("  Introducing\n   RaspiCalc")
 sleep(2)
 lcd.clear()
