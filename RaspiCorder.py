746e431df8b304099d718ca04e3c405e449a5c91
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2015-05-01 12:51:15 -0400

    intro screen

--- RaspiCorder.py
+++ RaspiCorder.py
@@ -3,5 +3,9 @@
 from time import sleep
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 
+# Initialization
 lcd = Adafruit_CharLCDPlate.Adafruit_CharLCDPlate()
 lcd.begin(16,2)
+lcd.message("  Introducing\n RaspiCorder")
+sleep(2)
+lcd.clear()
