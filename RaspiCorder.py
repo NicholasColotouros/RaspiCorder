c2a429497f1255763f94aa84d14029f571eaa710
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2015-05-01 12:55:30 -0400

    okay, stepwise removal for diagnosis

--- RaspiCorder.py
+++ RaspiCorder.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python
-
 from time import sleep
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 
 #Initialization of the plate
 lcd = Adafruit_CharLCDPlate.Adafruit_CharLCDPlate()
-lcd.begin(16,2)
-lcd.clear()
-lcd.message("  Introducing\n   RaspiCalc")
-sleep(2)
-lcd.clear()
